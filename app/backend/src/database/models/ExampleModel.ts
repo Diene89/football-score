7455340e45a97ef5952724b4a0bb85d7f3808460
Diene89 <diene_89@hotmail.com>
2022-08-17 21:37:22 -0300

    feat: cria Model do User

--- app/backend/src/database/models/ExampleModel.ts
+++ app/backend/src/database/models/ExampleModel.ts
@@ -1,11 +1,9 @@
 import { Model } from 'sequelize';
 import db from '.';
 // import OtherModel from './OtherModel';
-
 class Example extends Model {
   // public <campo>!: <tipo>;
 }
-
 Example.init({
   // ... Campos
 }, {
@@ -15,16 +13,12 @@ Example.init({
   // modelName: 'example',
   timestamps: false,
 });
-
 /**
-  * `Workaround` para aplicar as associations em TS: 
+  * `Workaround` para aplicar as associations em TS:
   * Associations 1:N devem ficar em uma das instâncias de modelo
   * */
-
 // OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
 // OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-
 // Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
 // Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
-
 export default Example;
