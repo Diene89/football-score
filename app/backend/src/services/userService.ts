92b09cf789a1bb72bb1c1d8d37d73fdda20f99bc
Diene89 <diene_89@hotmail.com>
2022-08-19 13:40:52 -0300

    feat: instala joi e express-async-errors

--- app/backend/src/services/userService.ts
+++ app/backend/src/services/userService.ts
@@ -7,12 +7,18 @@ export default class UserService implements IUser {
    email: string;
    password: string;
 
+   async validate
+
    static async login(user: IUser): Promise<string> {
 
         const dbUser: User | null = await User.findOne({
             where: { email: user.email, password: user.password }
         })
         if (!dbUser || !compare(dbUser.passaword, user.password)) throw new Error();
+    //     const e = new Error();
+    //   e.name = 'NotFoundError';
+    //   e.message = 'User not found';
+    //   throw e;
         const token = createToken.jwt(user)
         return token
    }
